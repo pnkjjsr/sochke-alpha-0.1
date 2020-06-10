570d688f91c125b690cc0ec25cef63d4e295c157
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-06-10 19:39:36 +0530

    master: nextjsconfig | commented code removed.

--- src/next.config.js
+++ src/next.config.js
@@ -9,17 +9,6 @@ const nextConfig = {
     CONTENTFUL_PREVIEW_ACCESS_TOKEN:
       process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
   },
-  // webpack(config, { buildId, dev, isServer, defaultLoaders }) {
-  //   // Alias
-  //   config.resolve.alias["pages"] = path.join(__dirname, "pages");
-  //   config.resolve.alias["public"] = path.join(__dirname, "public");
-  //   config.resolve.alias["layouts"] = path.join(__dirname, "src/layouts");
-  //   config.resolve.alias["libs"] = path.join(__dirname, "src/libs");
-  //   config.resolve.alias["components"] = path.join(__dirname, "src/components");
-  //   config.resolve.alias["utils"] = path.join(__dirname, "src/utils");
-  //   config.resolve.alias["common"] = path.join(__dirname, "src/common");
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
