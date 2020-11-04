5553f7d12999785a3b0270427e4b607b5e177c5a
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-11-04 20:33:24 +0530

    master(ref): logo link changes.

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Link from "next/link";
 
 import s from "./logo.module.scss";
 
@@ -6,7 +7,9 @@ export default class Logo extends Component {
   render() {
     return (
       <div className={s.logo}>
-        Sochke
+        <Link href="/">
+          <a>Sochke</a>
+        </Link>
         <span>alpha 0.1</span>
       </div>
     );
